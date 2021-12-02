c8a184fab78e5e98fbf64d12640c3143a73289af
irina-p-d <irinapetrovap@gmail.com>
2021-12-02 20:56:44 +0100

    working wid grid cards

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,35 @@
 <template>
-  <v-card class="mx-auto" max-width="200">
-    <v-container fluid>
-      <v-row>
-        <v-col v-for="card in cards" :key="card.tittle" :cols="flex">
-          <v-card>
-            <v-img
-              :src="card.src"
-              class="yellow--text align-end"
-              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-            >
-              <v-card-title v-text="card.title"></v-card-title>
-            </v-img>
+  <div style="width: 80%; border: 1px solid red; margin: 0 auto">
+    <v-card class="mx-auto" max-width="1200">
+      <v-container
+        fluid
+        style="border: 2px solid green; width: 100%; background: transparent"
+      >
+        <v-row dense>
+          <v-col v-for="card in cards" :key="card.tittle" :cols="card.flex">
+            <v-card>
+              <v-img
+                :src="card.src"
+                class="yellow--text align-end"
+                gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                height="200"
+              >
+                <v-card-title v-text="card.title"></v-card-title>
+              </v-img>
 
-            <v-card-actions>
-              <v-spacer></v-spacer>
+              <v-card-actions>
+                <v-spacer></v-spacer>
 
-              <v-btn icon>
-                <v-icon>mdi-heart</v-icon>
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-card>
+                <v-btn icon>
+                  <v-icon>mdi-heart</v-icon>
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card>
+  </div>
 </template>
 
 <script>
@@ -34,44 +40,37 @@ export default {
       {
         title: "1",
         src: require("@/assets/poder-darth-vader-960x720.jpg"),
-        flex: 12,
+        flex: 3,
       },
       {
-        title: "2",
+        title: "3",
         src: require("@/assets/poder-darth-vader-960x720.jpg"),
-        flex: 12,
+        flex: 3,
       },
       {
         title: "3",
         src: require("@/assets/WhatsApp Image 2021-11-23 at 21.16.49.jpeg"),
-        flex: 12,
+        flex: 3,
       },
       {
         title: "4",
         src: require("@/assets/poder-darth-vader-960x720.jpg"),
-        flex: 12,
-      },
-    ],
-    cards2: [
-      {
-        title: "5",
-        src: require("../assets/poder-darth-vader-960x720.jpg"),
-        flex: 12,
+        flex: 3,
       },
       {
-        title: "5",
-        src: require("../assets/poder-darth-vader-960x720.jpg"),
-        flex: 12,
+        title: "4",
+        src: require("@/assets/poder-darth-vader-960x720.jpg"),
+        flex: 3,
       },
       {
-        title: "5",
-        src: require("../assets/poder-darth-vader-960x720.jpg"),
-        flex: 12,
+        title: "4",
+        src: require("@/assets/poder-darth-vader-960x720.jpg"),
+        flex: 3,
       },
       {
-        title: "5",
-        src: require("../assets/poder-darth-vader-960x720.jpg"),
-        flex: 12,
+        title: "4",
+        src: require("@/assets/poder-darth-vader-960x720.jpg"),
+        flex: 3,
       },
     ],
   }),
